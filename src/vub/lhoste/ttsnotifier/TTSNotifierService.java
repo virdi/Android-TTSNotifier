fff4414e5a8adba66591564453bbec7fabc5c824
Zillode <zillode@zillode.be>
2009-08-23 22:15:58 +0200

    Special TTS ringtone + user defined texts

--- src/vub/lhoste/ttsnotifier/TTSNotifierService.java
+++ src/vub/lhoste/ttsnotifier/TTSNotifierService.java
@@ -1,5 +1,7 @@
 package vub.lhoste.ttsnotifier;
 
+import java.io.IOException;
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -7,6 +9,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.database.Cursor;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
@@ -17,6 +20,7 @@ import android.os.Message;
 import android.os.Process;
 import android.preference.PreferenceManager;
 import android.provider.Contacts;
+import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
@@ -35,25 +39,35 @@ public class TTSNotifierService extends Service {
 	private static final String ACTION_PICK_WIFI_NETWORK = "android.net.wifi.PICK_WIFI_NETWORK";
 	private static final String ACTION_SUPPLICANT_CONNECTION_CHANGE_ACTION = "android.net.wifi.supplicant.CONNECTION_CHANGE";
 
-	private static final int TTS_MAXTRIES = 30;
-	private static final int TTS_THREADWAIT = 300;
+	private static final int MEDIUM_THREADWAIT = 300;
+	private static final int FAST_THREADWAIT = 50;
 
 	private static TTS myTts = null;
-	private static boolean myTtsReady = false;
+	private MediaPlayer myRingTonePlayer = null;
+	private volatile boolean stopRingtone = false;
+	private volatile boolean ttsReady = false;
 
 	private Context context;
 	private ServiceHandler mServiceHandler;
 	private Looper mServiceLooper;
 
 	// Settings
+	private boolean cbxEnable;
 	private boolean cbxEnableIncomingCall;
 	private boolean cbxEnableWifiConnectDisconnect;
 	private boolean cbxEnableWifiDiscovered;
-	private boolean cbxDisableAll;
 	private boolean cbxObeySilentMode;
-	
+	private String txtOptionsIncomingCall;
+	private String txtOptionsWifiDiscovered;
+	private String txtOptionsWifiConnected;
+	private String txtOptionsWifiDisconnected;
+
 	// State
 	private boolean silentMode = false;
+	private boolean cbxOptionsIncomingCallUseTTSRingtone;
+	private String txtOptionsIncomingCallRingtone;
+	private Integer intOptionsIncomingCallMinimalRingCountTTSDelay;
+	private Thread myRingtoneThread;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -63,7 +77,8 @@ public class TTSNotifierService extends Service {
 	@Override
 	public void onCreate() {
 		Log.v("TTSNotifierService", "onCreate()");
-		HandlerThread thread = new HandlerThread("HandleIntentTTSNotifier", Process.THREAD_PRIORITY_BACKGROUND);
+		//HandlerThread thread = new HandlerThread("HandleIntentTTSNotifier", Process.THREAD_PRIORITY_BACKGROUND);
+		HandlerThread thread = new HandlerThread("HandleIntentTTSNotifier", Process.THREAD_PRIORITY_URGENT_AUDIO);
 		thread.start();
 		context = getApplicationContext();
 		mServiceLooper = thread.getLooper();
@@ -73,9 +88,8 @@ public class TTSNotifierService extends Service {
 	@Override
 	public void onStart(Intent intent, int startId) {
 		Log.v("TTSNotifierService", "onStart()");
-		if (myTts == null) {
-			myTts = new TTS(this, ttsInitListener, true);		
-		}
+		if (myTts == null)
+			myTts = new TTS(this, ttsInitListener, true);
 		Message msg = mServiceHandler.obtainMessage();
 		msg.arg1 = startId;
 		msg.obj = intent;
@@ -84,10 +98,13 @@ public class TTSNotifierService extends Service {
 
 	@Override
 	public void onDestroy() {
-		myTtsReady = false;
-		if (myTts != null) {
-			myTts.shutdown();			
-		}
+		ttsReady = false;
+		if (myTts != null) 
+			myTts.shutdown();
+		myTts = null;
+		if (myRingTonePlayer != null)
+			myRingTonePlayer.release();
+		myRingTonePlayer = null;
 		mServiceLooper.quit();
 	} 
 
@@ -106,9 +123,10 @@ public class TTSNotifierService extends Service {
 			readSettings();
 
 			if (action == null) return;
-			if (cbxDisableAll) return;
+			if (!cbxEnable) return;
 			if (cbxObeySilentMode && silentMode) return;
-			if (!waitForSpeak()) return;
+
+			Log.v("TTSNotifier", "Action: " + action);
 
 			if (ACTION_PHONE_STATE.equals(action)) {
 				handleACTION_PHONE_STATE(intent);
@@ -146,43 +164,153 @@ public class TTSNotifierService extends Service {
 	private void readSettings() {
 		// Preferences
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-		cbxDisableAll = prefs.getBoolean("cbxDisableAll", false);
+		cbxEnable = prefs.getBoolean("cbxEnable", false);
 		cbxEnableIncomingCall = prefs.getBoolean("cbxEnableIncomingCall", true);
 		cbxEnableWifiDiscovered = prefs.getBoolean("cbxEnableWifiDiscovered", true);
 		cbxEnableWifiConnectDisconnect = prefs.getBoolean("cbxEnableWifiConnectDisconnect", true);
 		cbxObeySilentMode = prefs.getBoolean("cbxObeySilentMode", true);
+		if (prefs.getBoolean("cbxOptionsIncomingCallUserDefinedText", false))
+			txtOptionsIncomingCall = prefs.getString("txtOptionsIncomingCall", "Phone call from %s");
+		else
+			txtOptionsIncomingCall = "Phone call from %s";
+		cbxOptionsIncomingCallUseTTSRingtone = prefs.getBoolean("cbxOptionsIncomingCallUseTTSRingtone", false);
+		txtOptionsIncomingCallRingtone = prefs.getString("txtOptionsIncomingCallRingtone", "DEFAULT_RINGTONE_URI");
+		intOptionsIncomingCallMinimalRingCountTTSDelay = Integer.parseInt(prefs.getString("intOptionsIncomingCallMinimalRingCountTTSDelay", "2"));
+		if (prefs.getBoolean("cbxOptionsWifiDiscoveredUserDefinedText", false))
+			txtOptionsWifiDiscovered = prefs.getString("txtOptionsWifiDiscovered", "Wyfy Signal in Range");
+		else
+			txtOptionsWifiDiscovered = "Wifi Signal in Range";
+		if (prefs.getBoolean("cbxOptionsWifiConnectedUserDefinedText", false))
+			txtOptionsWifiConnected = prefs.getString("txtOptionsWifiConnected", "Wyfy connected");
+		else
+			txtOptionsWifiConnected = "Wifi connected";
+		if (prefs.getBoolean("cbxOptionsWifiDisconnectedUserDefinedText", true))
+			txtOptionsWifiDisconnected = prefs.getString("txtOptionsWifiDisconnected", "Wyfy disconnected");
+		else
+			txtOptionsWifiDisconnected = "Wifi disconnected";
 		// State
 		AudioManager am = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
 		silentMode = am.getRingerMode() != AudioManager.RINGER_MODE_NORMAL;
 	}
 
 
-	private boolean waitForSpeak() {
-		try {
-			for (int i = 0; i < TTS_MAXTRIES; i++) {
-				Log.v("LODE", "waitForSpeak()");
-				if (myTtsReady) {
-					return true;
-				} else {
-					Thread.sleep(TTS_THREADWAIT);
-				}
+	private void waitForSpeechInitialised() {
+		while (!ttsReady) {
+			try {
+				Thread.sleep(MEDIUM_THREADWAIT);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
 		}
-		return false;
 	}
 
-	private void speak(String str) {
+	private void waitForSpeechFinished() {
+		do {
+			try {
+				Thread.sleep(MEDIUM_THREADWAIT);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} while (myTts.isSpeaking());
+	}
+
+	private void waitForRingTonePlayed() {
+		do {
+			try {
+				Thread.sleep(MEDIUM_THREADWAIT);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} while (myRingTonePlayer.isPlaying());
+	}
+
+	private void waitForRingToneThreadStopped() {
+		while (stopRingtone) {
+			try {
+				Thread.sleep(FAST_THREADWAIT);
+			} catch (InterruptedException e) { 
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void playRingtone(boolean waitForFinish) throws IllegalStateException, IOException {
+		myRingTonePlayer.start();
+		if (waitForFinish)
+			waitForRingTonePlayed();
+	}
+
+	private void speak(String str, boolean waitForFinish) {
+		waitForSpeechInitialised();
 		myTts.speak(str, 0, null);
+		if (waitForFinish)
+			waitForSpeechFinished();
 	}
 
 	private void handleACTION_PHONE_STATE(Intent intent) {
 		if (!cbxEnableIncomingCall) return;
-		TelephonyManager myTelManager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
-		if(myTelManager.getCallState() == TelephonyManager.CALL_STATE_RINGING) {
-			String phoneNr = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-			speak("Incoming call from " + getContactNameFromNumber(phoneNr));
+		TelephonyManager telManager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+		final String phoneNr = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+		if (myRingtoneThread != null)
+			stopRingtone = true;
+		if (myTts != null)
+			myTts.stop();
+		if (myRingTonePlayer != null) {
+			myRingTonePlayer.stop();
+			try {
+				myRingTonePlayer.prepare();
+			} catch (IllegalStateException e) {
+				myRingTonePlayer = null;
+			} catch (IOException e) {
+				myRingTonePlayer = null;
+			}
+		}
+		if(telManager.getCallState() == TelephonyManager.CALL_STATE_RINGING) {
+			waitForRingToneThreadStopped();
+			myRingtoneThread = new Thread() {
+				public void run() {
+					try {
+						if (cbxOptionsIncomingCallUseTTSRingtone) {
+							if (myRingTonePlayer == null)
+								myRingTonePlayer = MediaPlayer.create(context, Uri.parse(txtOptionsIncomingCallRingtone));
+							int ringtoneState = 0;
+							int ringCounter = 1;
+							while (!stopRingtone) {
+								Log.v("LODE", "CALL STATE :" + ringtoneState);
+								switch (ringtoneState) {
+								case 0:
+									playRingtone(true);
+									break;
+								case 1:
+									Log.v("LODE", "CALL STATE :" + ringCounter + " - "+intOptionsIncomingCallMinimalRingCountTTSDelay);
+									if (!ttsReady || ringCounter < intOptionsIncomingCallMinimalRingCountTTSDelay) {
+										ringCounter += 1;
+										ringtoneState = -1;
+									}
+									break;
+								case 2:
+									speak(String.format(txtOptionsIncomingCall, getContactNameFromNumber(phoneNr)), true);
+									break;
+								case 3:
+									ringtoneState = -1;
+									ringCounter = 1;
+									break;
+								}
+								ringtoneState += 1;
+							}
+						} else {
+							speak(String.format(txtOptionsIncomingCall, getContactNameFromNumber(phoneNr)), false);
+						}
+					} catch (IllegalStateException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					myRingtoneThread = null;
+					stopRingtone = false;
+				}
+			};
+			myRingtoneThread.start();
 		}
 	}
 
@@ -216,22 +344,24 @@ public class TTSNotifierService extends Service {
 
 	public void handleACTION_PICK_WIFI_NETWORK(Intent intent) {
 		if (!cbxEnableWifiDiscovered) return;
-		speak("Pick a Wi-Fi network to connect to");	
+		speak(txtOptionsWifiDiscovered, false);	
 	}
 
 	private void handleSUPPLICANT_CONNECTION_CHANGE_ACTION(Intent intent) {
 		if (!cbxEnableWifiConnectDisconnect) return;
+		Log.v("LODE", txtOptionsWifiConnected);
 		if (intent.getBooleanExtra(WifiManager.EXTRA_SUPPLICANT_CONNECTED, true)) {
-			speak("Wifi connected");			
+			speak(txtOptionsWifiConnected, false);			
 		} else {
-			speak("Wifi disconnected");
+			speak(txtOptionsWifiDisconnected, false);
 		}
 	}
 
 	private TTS.InitListener ttsInitListener = new TTS.InitListener() {
+		@Override
 		public void onInit(int version) {
-			Log.v("LODE", "INITDONE");
-			myTtsReady = true;
+			Log.v("TTSNotifier", "TTS INIT DONE");
+			ttsReady = true;
 		}
 	};
 
